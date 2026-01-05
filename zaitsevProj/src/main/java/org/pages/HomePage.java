d9778e86c7ce9cf41089d77c9928addf27fe42fa
Oleg <d1verr1123@gmail.com>
2026-01-05 20:14:56 +0200

    HomePage.java was refactored, in the CommonActionsWithElement.java was implemented new methods, in the LoginPage.java was implemented new methods, invalidLogin test was added

--- zaitsevProj/src/main/java/org/pages/HomePage.java
+++ zaitsevProj/src/main/java/org/pages/HomePage.java
@@ -4,17 +4,35 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends ParentPage{
     private Logger logger = Logger.getLogger(getClass());
+
+    @FindBy(xpath = "//button[text()='Sign Out']")
+    private WebElement buttonSignOut;
+
+    @FindBy(xpath = "//a[@href='/create-post']")
+    private WebElement buttonCreatePost;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void checkIsButtonSignOutVisible(){
-        Assert.assertTrue("Button SignOut is not visible", isButtonSignOutVisible());
+        Assert.assertTrue("Button SignOut is not visible", isElementDisplayed(buttonSignOut));
         logger.info("Button SignOut is visible");
+    }
+
+    public void checkIsButtonCreatePostVisible(){
+        Assert.assertTrue("Button CreatePost is not visible", isElementDisplayed(buttonCreatePost));
+        logger.info("Button CreatePost is visible");
+    }
+
+    public void checkIsButtonSignOutIsNotVisible(){
+        Assert.assertFalse("Button Sign Out is visible, but should not be", isElementDisplayed(buttonSignOut));
+        logger.info("Button Sign Out is not visible");
     }
 
      boolean isButtonSignOutVisible() {
