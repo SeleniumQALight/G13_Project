dfd529e808cab6f8f427ba91cd7dba7350d0ee33
vladpokydko <pokidko.vlad@gmail.com>
2026-01-08 21:53:43 +0200

    CreateNewPostTest part 2

--- pokydkoProject/src/test/java/org/postsTests/CreateNewPostTest.java
+++ pokydkoProject/src/test/java/org/postsTests/CreateNewPostTest.java
@@ -5,6 +5,19 @@ import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
     @Test
-    public void createNewPost(){
-        pageProvider.getLoginPage().openLoginPageAndFillLoginFormWithValidCred();
+    public void createNewPost() {
+        pageProvider.getLoginPage()
+                .openLoginPageAndFillLoginFormWithValidCred()
+                .checkIsRedirectToHomePage()
+                .clickOnButtonCreatePost()
+                .checkIsRedirectToCreatePostPage()
+                .enterTextIntoInputTitle("G13 Vladislav's post")
+                .enterTextIntoInputBody("G13 Vladislav's Some body text")
+                .selectTextInDropdownAccess("Приватне повідомлення")
+                .clickOnSaveNewPostButton()
+                .checkIsRedirectToPostPage()
+                .checkPostWasCreatedMessageIsDisplayed()
+                .checkTextInSuccessMessage("New post successfully created.")
+                .getHeaderForLoggedUserElement().clickOnButtonMyProfile();
+    }
 }
