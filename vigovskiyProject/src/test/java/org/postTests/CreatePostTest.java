bece6cfcf4a9088e4aad1ca40074bbb5fc2956eb
Yevhen V <centur.jv@gmail.com>
2026-01-08 22:06:45 +0200

    CW5 Create new post test part2

--- vigovskiyProject/src/test/java/org/postTests/CreatePostTest.java
+++ vigovskiyProject/src/test/java/org/postTests/CreatePostTest.java
@@ -7,7 +7,20 @@ public class CreatePostTest extends BaseTest {
     @Test
     public void createNewPost() {
 
-        pageProvider.getLoginPage().openLoginPageAndFillInLoginFormWithValidCred();
+        pageProvider.getLoginPage()
+                .openLoginPageAndFillInLoginFormWithValidCred()
+                .checkIsRedirectToHomePage()
+                .clickOnButtonCreatePost()
+                .checkIsRedirectToCreateNewPostPage()
+                .enterTextIntoInputTitle("G13 Jenya")
+                .enterTextIntoInputBody("G13 Jenya")
+                .selectTextInDropdownAccess("Приватне повідомлення")
+                .clickOnSaveNewPostButton()
+                .checkIsRedirectToPostPage()
+                .checkPostWasCreatedMessageIsDisplayed("G13 Jenya")
+                .checkTextInSuccessMessage("New post successfully created.")
+                .getHeaderForLoggedUserElement().clickOnButtonMyProfile();
+
 //        String title = "My first post";
 //        String body = "This is the body of my first post";
 //
