ce4e69cb07327aef4552bcf9b714fe3fbd5197f0
tradulenko <radulenko@gmail.com>
2026-01-08 21:54:19 +0200

    CreateNewPostTest part 2

---
+++ radulenkoProj/src/main/java/org/pages/elements/HeaderForLoggedUserElement.java
@@ -0,0 +1,20 @@
+package org.pages.elements;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.pages.CommonActionsWithElements;
+
+public class HeaderForLoggedUserElement extends CommonActionsWithElements {
+
+    @FindBy(xpath = "//img[@alt='My profile']")
+    private WebElement buttonMyProfile;
+
+    public HeaderForLoggedUserElement(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void clickOnButtonMyProfile() {
+        clickOnElement(buttonMyProfile);
+    }
+}
--- radulenkoProj/src/test/java/org/postsTests/CreateNewPostTest.java
+++ radulenkoProj/src/test/java/org/postsTests/CreateNewPostTest.java
@@ -4,9 +4,22 @@ import org.baseTest.BaseTest;
 import org.junit.Test;
 
 public class CreateNewPostTest extends BaseTest {
+
     @Test
     public void createNewPost(){
         pageProvider.getLoginPage()
-                .openLoginPageAndFillLoginFormWithValidCred();
+                .openLoginPageAndFillLoginFormWithValidCred()
+                .checkIsRedirectToHomePage()
+                .clickOnButtonCreatePost()
+                .checkIsRedirectToCreateNewPostPage()
+                .enterTextIntoInputTitle("G13 Taras")
+                .enterTextIntoInputBody("G13 Taras Some body")
+                .selectTextInDropdownAccess("Приватне повідомлення")
+
+                .clickOnSaveNewPostButton()
+                .checkIsRedirectToPostPage()
+                .checkPostWasCreatedMessageIsDisplayed()
+                .checkTextInSuccessMessage("New post successfully created.")
+                .getHeaderForLoggedUserElement().clickOnButtonMyProfile();
     }
 }
