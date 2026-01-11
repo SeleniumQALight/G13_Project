8d4681eb35887020d288a497ed9f8787ce9ce3c4
Nelly Koniushenko <k.v.nelly@gmail.com>
2026-01-11 23:22:02 +0100

    Create invalidLogi test and update validLogin test in LoginTestWithPageObject

--- koniushenkoProject/src/main/java/org/pages/HomePage.java
+++ koniushenkoProject/src/main/java/org/pages/HomePage.java
@@ -11,15 +11,38 @@ public class HomePage extends ParentPage {
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
+
     public void checkIsButtonSignOutVisible(){
         Assert.assertTrue("Button Sign Out is NOT visible",isButtonSingOutVisible());
         logger.info("Button Sign Out is visible");
+    }
+
+    public void checkIsButtonSignOutNotVisible(){
+        Assert.assertFalse("Button Sign Out is visible",isButtonSingOutVisible());
+        logger.info("Button Sign Out is not visible");
     }
 
     public boolean isButtonSingOutVisible() {
         try {
             boolean state = webDriver.findElement(By.xpath(
                     "//button[text()='Sign Out']")).isDisplayed();
+            logger.info("Element state:" + state);
+            return state;
+        } catch (Exception e) {
+            logger.info("Element is not found");
+            return false;
+        }
+    }
+
+    public void checkIsButtonCreatePostVisible(){
+        Assert.assertTrue("Button CreatePost is NOT visible",isButtonCreatePostVisible());
+        logger.info("Button CreatePost is visible");
+    }
+
+    public boolean isButtonCreatePostVisible() {
+        try {
+            boolean state = webDriver.findElement(By.xpath(
+                    "//a[text()='Create Post']")).isDisplayed();
             logger.info("Element state:" + state);
             return state;
         } catch (Exception e) {
--- koniushenkoProject/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ koniushenkoProject/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -11,5 +11,20 @@ public class LoginTestWithPageObject extends BaseTest {
         pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
         pageProvider.getLoginPage().clickOnButtonSignIn();
         pageProvider.getHomePage().checkIsButtonSignOutVisible();
+        pageProvider.getHomePage().checkIsButtonCreatePostVisible();
+        pageProvider.getLoginPage().checkIsInputLoginNotVisible();
+        pageProvider.getLoginPage().checkIsInputPasswordNotVisible();
+    }
+
+    @Test
+    public void invalidLogin(){
+        pageProvider.getLoginPage().openLoginPage();
+        pageProvider.getLoginPage().enterTextIntoInputLogin("qaauto123");
+        pageProvider.getLoginPage().enterTextIntoInputPassword("123456qwerty");
+        pageProvider.getLoginPage().clickOnButtonSignIn();
+        pageProvider.getLoginPage().checkIsButtonSingInVisible();
+        pageProvider.getLoginPage().checkIsTextErrorInvalidDataVisible();
+        pageProvider.getHomePage().checkIsButtonSignOutNotVisible();
+
     }
 }
