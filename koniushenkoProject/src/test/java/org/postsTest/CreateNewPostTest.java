f1438fbacc835b42c6a48ade280ff8d23366bb8c
Nelly Koniushenko <k.v.nelly@gmail.com>
2026-01-13 00:42:57 +0100

    CreateNewPostTest part2

--- koniushenkoProject/src/test/java/org/postsTest/CreateNewPostTest.java
+++ koniushenkoProject/src/test/java/org/postsTest/CreateNewPostTest.java
@@ -7,7 +7,18 @@ public class CreateNewPostTest extends BaseTest {
     @Test
     public void createNewPostTest() {
         pageProvider.getLoginPage()
-                .openLoginPageAndFillLoginFormWithValidCred();
+                .openLoginPageAndFillLoginFormWithValidCred()
+                .checkIsRedirectToHomePage()
+                .clickOnButtonCreatePost()
+                .checkIsRedirectToCreateNewPostPage()
+                .enterTextInInputTitle("G13 Nelly Title")
+                .enterTextInInputBody("G13 Nelly Body")
+                .selectTextInDropdownAccess("Приватне повідомлення")
+                .clickOnSaveNewPostButton()
+                .chechIsRedirectToPostPage()
+                .checkPostWasCreatedMessageIsDisplayed()
+                .checkTextInSuccessMessage("New post successfully created.")
+                .getHederForLoggedUserElement().clickOnButtonMyProfile();
 
     }
 }
