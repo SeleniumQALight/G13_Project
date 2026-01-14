c9818fbe14af0d9e42a76bfe29e7704cfecbf2d7
Moon4779 <dzhobolda23@gmail.com>
2026-01-14 18:32:40 +0100

    Last Changes 14.06 HW 3

--- dzhoboldaTProject/src/main/java/org/pages/CommonActionsWithElements.java
+++ dzhoboldaTProject/src/main/java/org/pages/CommonActionsWithElements.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class CommonActionsWithElements {
     protected WebDriver webDriver;
@@ -34,6 +35,59 @@ public class CommonActionsWithElements {
         }
     }
 
+    protected void selectTextInDropDown(WebElement webElement, String text) {
+        try {
+            Select  select = new Select(webElement);
+            select.selectByVisibleText(text);
+            logger.info(text + " was selected in DropDown");
+        } catch (Exception e) {
+            printErrorAndStopTest();
+        }
+    }
+    protected void selectValueInDropDown(WebElement webElement, String value) {
+        try {
+            Select  select = new Select(webElement);
+            select.selectByValue(value);
+            logger.info(value + " was selected in DropDown");
+        } catch (Exception e) {
+            printErrorAndStopTest();
+        }
+    }
+    protected void checkIsElementEnabled(WebElement webElement) {
+        Assert.assertTrue("Element is not enabled", webElement.isEnabled());
+        logger.info("Element is enabled");
+    }
+
+    protected boolean isElementEnabled(WebElement webElement) {
+        try {
+            boolean state = webElement.isEnabled();
+            logger.info("Element enabled state: " + state);
+            return state;
+        } catch (Exception e) {
+            printErrorAndStopTest();
+            return false;
+        }
+    }
+    protected boolean isElementDisplayed(WebElement webElement) {
+        try {
+            boolean isDisplayed = webElement.isDisplayed();
+            logger.info("Element displayed - " + isDisplayed);
+            return isDisplayed;
+        } catch (Exception e) {
+            logger.info("Element is not displayed ");
+            return false;
+        }
+    }
+
+    protected void checkTextInElement(WebElement webElement, String text) {
+        try {
+            String actualText = webElement.getText();
+            Assert.assertEquals("Text in element is not as expected", text, actualText);
+            logger.info("Text in element is as expected: " + text);
+        } catch (Exception e) {
+            printErrorAndStopTest();
+        }
+    }
 
     private void printErrorAndStopTest() {
         logger.error("Error while working with element ");
--- dzhoboldaTProject/src/main/java/org/pages/HomePage.java
+++ dzhoboldaTProject/src/main/java/org/pages/HomePage.java
@@ -4,27 +4,82 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage  extends ParentPage{
     private Logger logger = Logger.getLogger(getClass());
+    // Элементы страницы
+    @FindBy(xpath = "//a[text()='Create Post']")
+    private WebElement createNewPostButton;
+
+    @FindBy(xpath = "//button[text()='Sign Out']")
+    private WebElement signOutButton;
+
+    @FindBy(xpath = "//button[text()='Sign In']")
+    private WebElement signInButton;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
+        PageFactory.initElements(webDriver, this); // инициализация элементов через @FindBy
     }
 
-    public void checkIsButtonSignOutVisible(){
+    // Сигнатура метода остаётся, но используем метод из CommonActionsWithElements
+    public boolean isButtonSignOutVisible() {
+        boolean state = isElementDisplayed(signOutButton);
+        logger.info("Button Sign Out displayed: " + state);
+        return state;
+    }
+
+    public boolean isButtonSignInDisplayed() {
+        boolean state = isElementDisplayed(signInButton);
+        logger.info("Button Sign In displayed: " + state);
+        return state;
+    }
+
+    public boolean isButtonCreatePostDisplayed() {
+        boolean state = isElementDisplayed(createNewPostButton);
+        logger.info("Button Create Post displayed: " + state);
+        return state;
+    }
+
+    // Сохраняем старый метод
+    public void checkIsButtonSignOutVisible() {
         Assert.assertTrue("Button SignOut is not visible", isButtonSignOutVisible());
         logger.info("Button SignOut is visible");
     }
 
-    public boolean isButtonSignOutVisible() {
-        try {
-            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
-            logger.info("Element sate: " + state);
-            return state;
-        }catch (Exception e){
-            logger.info("Element is not found");
-            return false;
-        }
+    public HomePage checkRedirectToHomePage() {
+        // TODO Check URL
+        // TODO Check unique element
+        return this;
     }
+
+//    public CreatePostPage clickOnButtonCreatePost() {
+//        clickOnElement(createNewPostButton);
+//        logger.info("Button Create Post was clicked");
+//        return new CreatePostPage(webDriver);
+//    }
+
+
+//    public HomePage(WebDriver webDriver) {
+//        super(webDriver);
+//    }
+//
+//    public void checkIsButtonSignOutVisible(){
+//        Assert.assertTrue("Button SignOut is not visible", isButtonSignOutVisible());
+//        logger.info("Button SignOut is visible");
+//    }
+//
+//    public boolean isButtonSignOutVisible() {
+//        try {
+//            boolean state = webDriver.findElement(By.xpath("//button[text()='Sign Out']")).isDisplayed();
+//            logger.info("Element sate: " + state);
+//            return state;
+//        }catch (Exception e){
+//            logger.info("Element is not found");
+//            return false;
+//        }
+//    }
 }
