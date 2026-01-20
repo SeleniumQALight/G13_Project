0c08dc48771533bc7b2564bfc35e48cb706efb7f
Moon4779 <dzhobolda23@gmail.com>
2026-01-20 20:49:44 +0100

    Last Changes 20.01

--- dzhoboldaTProject/src/main/java/org/pages/PostPage.java
+++ dzhoboldaTProject/src/main/java/org/pages/PostPage.java
@@ -42,7 +42,7 @@ public class PostPage extends ParentPage {
 
 
     public MyProfilePage clickOnDeleteButton() {
-        clickOnElement(buttonDeletePost);
+        clickOnElement(buttonDeletePost, "Delete Post Button");
         return new MyProfilePage(webDriver);
     }
 }
