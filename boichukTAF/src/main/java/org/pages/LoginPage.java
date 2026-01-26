c70c4d3019051adf2885c7a89633a20af9ac0ecd
Olena Boichuk <alyona.boychuk@gmail.com>
2026-01-26 22:43:32 +0200

    added new classes(ValidationMessages and RegistrationValidationMessages)

--- boichukTAF/src/main/java/org/pages/LoginPage.java
+++ boichukTAF/src/main/java/org/pages/LoginPage.java
@@ -1,11 +1,22 @@
 package org.pages;
 
 import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.data.RegistrationValidationMessages;
 import org.data.TestData;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.pages.utils.Utils_Custom;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.data.RegistrationValidationMessages.SEMICOLON;
 
 public class LoginPage extends ParentPage {
     private Logger logger = Logger.getLogger(getClass());
@@ -16,11 +27,27 @@ public class LoginPage extends ParentPage {
     @FindBy(xpath = "//input[@placeholder='Password']")
     private WebElement inputPassword;
 
+    @FindBy(xpath = "//div[text() = 'Invalid username/password.']")
+    private WebElement invalidUsernamePasswordMessage;
+
     @FindBy(xpath = "//button[text()='Sign In']")
     private WebElement buttonSignIn;
 
     @FindBy(xpath = "//div[@class='alert alert-danger text-center']")
     private WebElement invalidLoginMessage;
+
+    @FindBy(xpath = "//input[@id='username-register']")
+    private WebElement inputUserNameRegistrationForm;
+
+    @FindBy(xpath = "//input[@id='email-register']")
+    private WebElement inputEmailRegistrationForm;
+
+    @FindBy(xpath = "//input[@id='password-register']")
+    private WebElement inputPasswordRegistrationForm;
+
+    final static String listOfActualMessagesLocator = "//*[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']";
+    @FindBy(xpath = listOfActualMessagesLocator)
+    private List<WebElement> listOfActualMessages;
 
 
     public LoginPage(WebDriver webDriver) {
@@ -32,13 +59,13 @@ public class LoginPage extends ParentPage {
         return "/";
     }
 
-    public LoginPage openLoginPage(){
+    public LoginPage openLoginPage() {
         webDriver.get(baseUrl);
         logger.info("Login page was opened with url " + baseUrl);
         return this;
     }
 
-    public LoginPage enterTextIntoInputLogin (String text){
+    public LoginPage enterTextIntoInputLogin(String text) {
 //    WebElement inputLogin = webDriver.findElement(
 //            By.xpath("//input[@placeholder='Username']"));
 //        inputLogin.clear();
@@ -48,12 +75,12 @@ public class LoginPage extends ParentPage {
         return this;
     }
 
-    public LoginPage enterTextIntoInputPassword (String text){
+    public LoginPage enterTextIntoInputPassword(String text) {
         clearAndEnterTextIntoElement(inputPassword, text);
         return this;
     }
 
-    public void clickOnButtonSignIn(){
+    public void clickOnButtonSignIn() {
         clickOnElement(buttonSignIn);
     }
 
@@ -105,4 +132,42 @@ public class LoginPage extends ParentPage {
         }
     }
 
+    public LoginPage enterTextIntoRegistrationUserNameField(String userName) {
+        clearAndEnterTextIntoElement(inputUserNameRegistrationForm, userName);
+        return this;
+    }
+
+    public LoginPage enterTextIntoRegistrationEmailField(String email) {
+        clearAndEnterTextIntoElement(inputEmailRegistrationForm, email);
+        return this;
+    }
+
+    public LoginPage enterTextIntoRegistrationPasswordField(String password) {
+        clearAndEnterTextIntoElement(inputPasswordRegistrationForm, password);
+        return this;
+    }
+
+    public LoginPage checkErrorsMessages(String expectedMessages) {
+        //error1;error2;error3 -> [error1,error2,error3]
+        String[] expectedErrorsArray = expectedMessages.split(SEMICOLON);
+
+        webDriverWait10.withMessage("Error messages")
+                .until(ExpectedConditions.numberOfElementsToBe(By.xpath(listOfActualMessagesLocator),
+                        expectedErrorsArray.length));
+        Assert.assertEquals("Number of error messages", expectedErrorsArray.length, listOfActualMessages.size());
+
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        for (int i = 0; i < expectedErrorsArray.length; i++) {
+            softAssertions
+                    .assertThat(listOfActualMessages.get(i).getText())
+                    .as("Message number" + (i + 1))
+                    .isIn(expectedErrorsArray);
+        }
+        Utils_Custom.waitABit(1);
+
+        softAssertions.assertAll();
+        return this;
+
+    }
 }
