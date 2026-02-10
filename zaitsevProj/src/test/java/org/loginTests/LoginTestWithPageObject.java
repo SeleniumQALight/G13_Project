ff8a3c2ae49a098f46a3e221512e332c0e805350
Oleg <d1verr1123@gmail.com>
2026-02-10 20:49:21 +0200

    Suites

--- zaitsevProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ zaitsevProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,13 +1,16 @@
 package org.loginTests;
 
 import org.baseTest.BaseTest;
+import org.categories.SmokeTestsFilter;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.data.TestData.VALID_LOGIN;
 import static org.data.TestData.VALID_PASSWORD;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
+    @Category(SmokeTestsFilter.class)
     public void validLogin() {
         pageProvider.getLoginPage()
                 .openLoginPage()
