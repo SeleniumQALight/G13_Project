cb6a360dba3abeaa37361f0e8c0b0c74ed4dfee4
Moon4779 <dzhobolda23@gmail.com>
2026-02-10 20:58:35 +0100

    Changes Allure were added

--- dzhoboldaTProject/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ dzhoboldaTProject/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,5 +1,6 @@
 package org.loginTests;
 
+import io.qameta.allure.Step;
 import org.baseTest.BaseTest;
 import org.categories.SmokeTestsFilter;
 import org.junit.Test;
@@ -11,6 +12,7 @@ import static org.data.TestData.ValidPassword;
 public class LoginTestWithPageObject extends BaseTest {
         @Test
         @Category(SmokeTestsFilter.class)
+
         public void validLogin(){
             pageProvider.getLoginPage()
                     .openLoginPage()
