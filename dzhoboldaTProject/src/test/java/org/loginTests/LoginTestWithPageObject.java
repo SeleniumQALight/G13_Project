7951fdb59ad45d0d80812f146eb0f70e426b05f5
Moon4779 <dzhobolda23@gmail.com>
2026-02-10 19:49:08 +0100

    Changes Suits were added

--- dzhoboldaTProject/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ dzhoboldaTProject/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,13 +1,16 @@
 package org.loginTests;
 
 import org.baseTest.BaseTest;
+import org.categories.SmokeTestsFilter;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.data.TestData.ValidLogin;
 import static org.data.TestData.ValidPassword;
 
 public class LoginTestWithPageObject extends BaseTest {
         @Test
+        @Category(SmokeTestsFilter.class)
         public void validLogin(){
             pageProvider.getLoginPage()
                     .openLoginPage()
