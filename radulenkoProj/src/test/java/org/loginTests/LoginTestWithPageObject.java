bedc992197ed796932af9cfe5b95dc87e29940cf
tradulenko <radulenko@gmail.com>
2026-02-10 20:49:08 +0200

    Suites

--- radulenkoProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ radulenkoProj/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,14 +1,30 @@
 package org.loginTests;
 
 import org.baseTest.BaseTest;
+import org.categories.SmokeTestsFilter;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.data.TestData.VALID_LOGIN_UI;
 import static org.data.TestData.VALID_PASSWORD_UI;
 
 public class LoginTestWithPageObject extends BaseTest {
     @Test
+    @Category(SmokeTestsFilter.class)
     public void TC02_validLogin(){
+        pageProvider.getLoginPage()
+                .openLoginPage()
+                .enterTextIntoInputLogin(VALID_LOGIN_UI+11)
+                .enterTextIntoInputPassword(VALID_PASSWORD_UI)
+                .clickOnButtonSignIn();
+
+        pageProvider.getHomePage().checkIsButtonSignOutVisible();
+    }
+
+    @Test
+   @Ignore("This test is ignored because of some reasons")
+    public void TC044_validLogin(){
         pageProvider.getLoginPage()
                 .openLoginPage()
                 .enterTextIntoInputLogin(VALID_LOGIN_UI)
