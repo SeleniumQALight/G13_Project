981751f437a2932bce517bbda8edc61a6a577b5e
Olena Boichuk <alyona.boychuk@gmail.com>
2026-02-18 12:05:10 +0200

    Jenkins

--- boichukTAF/src/test/java/org/loginTests/LoginTestWithPageObject.java
+++ boichukTAF/src/test/java/org/loginTests/LoginTestWithPageObject.java
@@ -1,17 +1,25 @@
+
 package org.loginTests;
 
+import io.qameta.allure.*;
 import org.baseTest.BaseTest;
-import org.categories.SmokeTestsFilter;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 
 import static org.data.TestData.VALID_LOGIN;
 import static org.data.TestData.VALID_PASSWORD;
 
+
+@Epic("Allure examples")
+@Feature("Junit 4 support")
 public class LoginTestWithPageObject extends BaseTest {
 
     @Test
-    @Category(SmokeTestsFilter.class)
+    @Description("Some detailed test description")
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Issue("123")
+    @Issue("432")
+    @Story("Base support for bdd annotations")
     public void TC02_validLogin() {
         pageProvider.getLoginPage()
                 .openLoginPage()
