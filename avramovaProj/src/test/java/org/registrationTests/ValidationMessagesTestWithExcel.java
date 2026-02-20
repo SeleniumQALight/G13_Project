7baada622fc06d2ddc9f23b8f794a192a205af4b
yevhen-avramov <yevhen.avramov@gmail.com>
2026-02-20 16:34:17 +0200

    cw11-29-01

---
+++ avramovaProj/src/test/java/org/registrationTests/ValidationMessagesTestWithExcel.java
@@ -0,0 +1,38 @@
+package org.registrationTests;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.baseTest.BaseTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.utils.ConfigProvider;
+import org.utils.ExcelSpreadsheetData;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
+
+@RunWith(JUnitParamsRunner.class)
+public class ValidationMessagesTestWithExcel extends BaseTest {
+
+    @Test
+    @Parameters(method = "parametersForTestValidationMessages")
+    public void TC03_testValidationMessages(
+            String userName, String email, String password, String expectedMessages) {
+        pageProvider.getLoginPage().openLoginPage()
+                .enterTextIntoRegistrationUserNameField(userName)
+                .enterTextIntoRegistrationEmailField(email)
+                .enterTextIntoRegistrationPasswordField(password)
+                .checkErrorMessages(expectedMessages);
+    }
+
+    public Collection parametersForTestValidationMessages() throws IOException {
+        String pathToDataFile = ConfigProvider.configProperties.DATA_FILE_PATH() + "testDataSuit.xls";
+        String sheetName = "registrationErrors";
+        boolean skipFirstRow = false;
+        logger.info("pathToDataFile = " + pathToDataFile);
+        logger.info("sheetName = " + sheetName);
+        logger.info("skipFirstRow = " + skipFirstRow);
+        return new ExcelSpreadsheetData(new FileInputStream(pathToDataFile), sheetName, skipFirstRow).getData();
+    }
+}
