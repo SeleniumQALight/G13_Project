1ff72f8e06315cbc666c5447b00f532df79671e1
Yevhen V <centur.jv@gmail.com>
2026-03-05 21:43:41 +0200

    added api helper and dto

--- vigovskiyProject/src/main/java/org/apiTests/ApiTest.java
+++ vigovskiyProject/src/main/java/org/apiTests/ApiTest.java
@@ -1,23 +1,90 @@
 package org.apiTests;
 
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
+import org.api.ApiHelper;
 import org.api.EndPoints;
+import org.api.dto.responseDto.AuthorDto;
+import org.api.dto.responseDto.PostsDto;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
+import org.junit.jupiter.api.Assertions;
 
-public class ApiTest extends BaseTestApi{
-    String sharedUserName="qataras";
+import java.util.Arrays;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
+
+public class ApiTest extends BaseTestApi {
+    String sharedUserName = "autoapi";
+    Logger logger = Logger.getLogger(getClass());
+    ApiHelper apiHelper=new ApiHelper();
 
     @Test
-    public void getAllPostsByUser(){
-        given()
+    public void getAllPostsByUser() {
+        PostsDto[] actualResponse = given()
                 .contentType(ContentType.JSON)
                 .log().all()
                 .when()
-                .get(EndPoints.POSTS_BY_USER,sharedUserName)
+                .get(EndPoints.POSTS_BY_USER, sharedUserName)
                 .then()
                 .statusCode(200)
-                .log().all();
+                .log().all()
+                //method#1 RestAssured asserts
+                .body("[0].title", equalTo("The second Default post"))
+                .body("author.username", everyItem(equalTo(sharedUserName)))
+                //method#2 DTO or POJO опис джава об'екту
+                .extract().body().as(PostsDto[].class);
+
+        logger.info("full response body = " + Arrays.toString(actualResponse));
+        logger.info("number of posts = " + actualResponse.length);
+        logger.info("title[0] = " + actualResponse[0].getTitle());
+        logger.info("[0]UserName = " + actualResponse[0].getAuthor().getUsername());
+
+        for (int i = 0; i < actualResponse.length; i++) {
+            Assert.assertEquals("UserName is not expexted in post " + i,
+                    sharedUserName,
+                    actualResponse[i].getAuthor().getUsername());
+        }
+        PostsDto[] expectedResult = {
+                new PostsDto("The second Default post",
+                        "This post was created automatically after cleaning the database",
+                        "All Users",
+                        "no",
+                        new AuthorDto(sharedUserName),
+                        false),
+                new PostsDto("The first Default post",
+                        "This post was created automatically after cleaning the database",
+                        "All Users",
+                        "no",
+                        new AuthorDto(sharedUserName),
+                        false),
+
+        };
+        SoftAssertions softAssertions = new SoftAssertions();
+
+        softAssertions
+                .assertThat(actualResponse)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "createdDate" , "author.avatar")
+                .isEqualTo(expectedResult);
+
+
+        softAssertions.assertAll();
+
+    }
+
+    @Test
+    public void getAllPostsByUserNegative(){
+        final String NOT_VALID_USER_NAME="NotValidUser";
+        String actualResult=apiHelper.getAllPostsByUserRequest(NOT_VALID_USER_NAME, HttpStatus.SC_BAD_REQUEST)
+                .extract().response().body().asString();
+
+        Assert.assertEquals("Message in response",
+                "\"Sorry, invalid user requested. Wrong username - "+NOT_VALID_USER_NAME+" or there is no posts. Exception is undefined\"",actualResult);
     }
 }
