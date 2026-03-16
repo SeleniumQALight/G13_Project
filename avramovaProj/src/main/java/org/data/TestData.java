d8fd196af8e2b3a74585b56e51f52d330208b6d4
yevhen-avramov <yevhen.avramov@gmail.com>
2026-03-17 00:51:31 +0200

    сw3-12.03-lombok

--- avramovaProj/src/main/java/org/data/TestData.java
+++ avramovaProj/src/main/java/org/data/TestData.java
@@ -2,8 +2,13 @@ package org.data;
 
 import org.utils.ConfigProvider;
 
+import java.util.Locale;
+
 public class TestData {
     public final static String VALID_LOGIN_UI = System.getProperty("defaultLogin",
             ConfigProvider.configHiddenProperties.login());
     public final static String VALID_PASSWORD_UI = "123456qwerty";
+
+    public final static String VALID_USERNAME_API = "qaavramova".toLowerCase();
+    public final static String VALID_PASSWORD_API = "qaavramova123";
 }
