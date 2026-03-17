de57cb1bf4170b86fe95b42aac275b9de2152099
tradulenko <radulenko@gmail.com>
2026-03-17 20:20:16 +0200

    CreateNewPostDto delete post was added

--- radulenkoProj/src/main/java/org/api/ApiHelper.java
+++ radulenkoProj/src/main/java/org/api/ApiHelper.java
@@ -8,38 +8,43 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
 import org.api.dto.responseDto.PostsDto;
 import org.data.TestData;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
 
 public class ApiHelper {
-      public static RequestSpecification requestSpecification = new RequestSpecBuilder()
-              .setContentType(ContentType.JSON)
-              .log(LogDetail.ALL)
-              .build();
+    Logger logger = Logger.getLogger(getClass());
 
-      public static ResponseSpecification responseSpecification = new ResponseSpecBuilder()
-              .log(LogDetail.ALL)
-              .expectStatusCode(HttpStatus.SC_OK)
-              .build();
+    public static RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+            .log(LogDetail.ALL)
+            .expectStatusCode(HttpStatus.SC_OK)
+            .build();
 
 
-        public ValidatableResponse getAllPostsByUserRequest(String userName, int expectedStatusCode){
-            return given()
+    public ValidatableResponse getAllPostsByUserRequest(String userName, int expectedStatusCode) {
+        return given()
 //                    .contentType(ContentType.JSON)
 //                    .log().all()
-                    .spec(requestSpecification)
-                    .when()
-                    .get(EndPoints.POSTS_BY_USER, userName)
-                    .then()
-                    .log().all()
-                    .statusCode(expectedStatusCode);
-        }
+                .spec(requestSpecification)
+                .when()
+                .get(EndPoints.POSTS_BY_USER, userName)
+                .then()
+                .log().all()
+                .statusCode(expectedStatusCode);
+    }
 
     public String getToken() {
-            return getToken(TestData.VALID_USERNAME_API, TestData.VALID_PASSWORD_API);
+        return getToken(TestData.VALID_USERNAME_API, TestData.VALID_PASSWORD_API);
     }
 
     public String getToken(String userName, String password) {
@@ -56,16 +61,45 @@ public class ApiHelper {
 //                .statusCode(HttpStatus.SC_OK)
 //                .log().all()
                 .spec(responseSpecification)
-                .extract().body().asString().replace("\"","");
-
-
+                .extract().body().asString().replace("\"", "");
 
 
     }
 
     public PostsDto[] getAllPostsByUserInObject() {
-        return getAllPostsByUserRequest(TestData.VALID_USERNAME_API, HttpStatus.SC_OK)
+        return getAllPostsByUserInObject(TestData.VALID_USERNAME_API, HttpStatus.SC_OK);
+    }
+
+    public PostsDto[] getAllPostsByUserInObject(String userName, int status) {
+        return getAllPostsByUserRequest(userName, status)
                 .extract().body()
                 .as(PostsDto[].class);
     }
+
+    public void deleteAllPostsTillPresent(String validUsernameApi, String actualToken) {
+        PostsDto[] listOfPosts = this.getAllPostsByUserInObject(validUsernameApi, HttpStatus.SC_OK);
+
+        for (int i = 0; i < listOfPosts.length; i++) {
+            deletePostById(actualToken, listOfPosts[i].getId());
+            logger.info(
+                    String.format("Post with id %s and title %s was deleted "
+                    ,listOfPosts[i].getId() , listOfPosts[i].getTitle()));
+        }
+
+    }
+
+    private void deletePostById(String actualToken, String id) {
+        HashMap<String, String> bodyRequest = new HashMap<>();
+        bodyRequest.put("token", actualToken);
+
+        given()
+                .spec(requestSpecification)
+                .body(bodyRequest)
+                .when()
+                .delete(EndPoints.DELETE_POST, id)
+                .then()
+                .spec(responseSpecification);
+    }
+
+
 }
