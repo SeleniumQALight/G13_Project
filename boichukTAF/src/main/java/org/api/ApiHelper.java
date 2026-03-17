50e1ff62667542d18a9961879e88ef1ca1289dad
Olena Boichuk <alyona.boychuk@gmail.com>
2026-03-17 20:20:23 +0200

    CreateNewPostDTO delete post was added

--- boichukTAF/src/main/java/org/api/ApiHelper.java
+++ boichukTAF/src/main/java/org/api/ApiHelper.java
@@ -9,13 +9,18 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
 import org.api.dto.responseDto.PostsDto;
 import org.data.TestData;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
 
 public class ApiHelper {
+    Logger logger = Logger.getLogger(getClass());
+
     public static RequestSpecification requestSpecification = new RequestSpecBuilder()
             .setContentType(ContentType.JSON)
             .log(LogDetail.ALL)
@@ -27,7 +32,7 @@ public class ApiHelper {
             .build();
 
 
-    public ValidatableResponse getAllPostsByUserRequest(String userName, int expectedStatusCode){
+    public ValidatableResponse getAllPostsByUserRequest(String userName, int expectedStatusCode) {
         return given()
 //                .contentType(ContentType.JSON)
 //                .log().all()
@@ -39,11 +44,11 @@ public class ApiHelper {
                 .statusCode(expectedStatusCode);
     }
 
-    public String getToken(){
+    public String getToken() {
         return getToken(TestData.VALID_USERNAME_API, TestData.VALID_PASSWORD_API);
     }
 
-    public String getToken(String userName, String password){
+    public String getToken(String userName, String password) {
         JSONObject requestBody = new JSONObject();
         requestBody.put("username", userName);
         requestBody.put("password", password);
@@ -57,15 +62,45 @@ public class ApiHelper {
 //                .statusCode(HttpStatus.SC_OK)
 //                .log().all()
                 .spec(responseSpecification)
-                .extract().body().asString().replace("\"","");
+                .extract().body().asString().replace("\"", "");
 
 
     }
 
+
     public PostsDto[] getAllPostsByUserInObject() {
-        return getAllPostsByUserRequest(TestData.VALID_USERNAME_API, HttpStatus.SC_OK)
+        return getAllPostsByUserInObject(TestData.VALID_USERNAME_API, HttpStatus.SC_OK);
+
+    }
+
+    public PostsDto[] getAllPostsByUserInObject(String userName, int status) {
+        return getAllPostsByUserRequest(userName, status)
                 .extract()
                 .body()
                 .as(PostsDto[].class);
+    }
+
+    public void deleteAllPostsTillPresent(String validUsernameApi, String actualToken) {
+        PostsDto[] listOfPosts = this.getAllPostsByUserInObject(validUsernameApi, HttpStatus.SC_OK);
+
+        for (int i = 0; i < listOfPosts.length; i++) {
+            deletePostById(actualToken, listOfPosts[i].getId());
+            logger.info(
+                    String.format("Post withId %s and title %s was deleted ",
+                            listOfPosts[i].getId(), listOfPosts[i].getTitle()));
+        }
+    }
+
+    private void deletePostById(String actualToken, String id) {
+        HashMap<String, String> bodyRequest = new HashMap<>();
+        bodyRequest.put("token", actualToken);
+
+        given()
+                .spec(requestSpecification)
+                .body(bodyRequest)
+                .when()
+                .delete(EndPoints.DELETE_POST, id)
+                .then()
+                .spec(responseSpecification);
     }
 }
