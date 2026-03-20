347c21e1a871323454c24c246a7396084abd9979
Moon4779 <dzhobolda23@gmail.com>
2026-03-20 12:40:10 +0100

    HW-1-BANK was changed

--- dzhoboldaTProject/src/test/java/org/apiTests/PrivatBankTests.java
+++ dzhoboldaTProject/src/test/java/org/apiTests/PrivatBankTests.java
@@ -29,14 +29,44 @@ public class PrivatBankTests extends BaseTestApi {
 
         softAssertions.assertThat(response.getDate()).isEqualTo(date);
         softAssertions.assertThat(response.getBank()).isEqualTo("PB");
+        softAssertions.assertThat(response.getBaseCurrencyLit()).isEqualTo("UAH");
 
-        // Цикл для проверки валют (baseCurrency и currency)
+        // Переделанный цикл
         response.getExchangeRate().forEach(rate -> {
-            softAssertions.assertThat(rate.getBaseCurrency()).isEqualTo("UAH");
-            softAssertions.assertThat(rate.getCurrency()).isNotNull();
+            softAssertions.assertThat(rate.getBaseCurrency())
+                    .as("Base currency for " + rate.getCurrency())
+                    .isEqualTo("UAH");
+
+            //  название валюты (currency) не пустое
+            softAssertions.assertThat(rate.getCurrency())
+                    .as("Currency field is null")
+                    .isNotNull();
+
+            //  все курсы > 0
+
+            validateRateGreaterThanZero(softAssertions, "saleRateNB", rate.getSaleRateNB());
+            validateRateGreaterThanZero(softAssertions, "purchaseRateNB", rate.getPurchaseRateNB());
+            validateRateGreaterThanZero(softAssertions, "saleRate", rate.getSaleRate());
+            validateRateGreaterThanZero(softAssertions, "purchaseRate", rate.getPurchaseRate());
         });
+
+//        // Цикл для проверки валют (baseCurrency и currency)
+//        response.getExchangeRate().forEach(rate -> {
+//            softAssertions.assertThat(rate.getBaseCurrency()).isEqualTo("UAH");
+//            softAssertions.assertThat(rate.getCurrency()).isNotNull();
+//        });
 //
 
         softAssertions.assertAll();
+
+    }
+
+
+    private void validateRateGreaterThanZero(SoftAssertions softAssertions, String fieldName, Double value) {
+        if (value != null) {
+            softAssertions.assertThat(value)
+                    .as("Field " + fieldName + " should be greater than 0, but was " + value)
+                    .isGreaterThan(0);
     }
 }
+    }
