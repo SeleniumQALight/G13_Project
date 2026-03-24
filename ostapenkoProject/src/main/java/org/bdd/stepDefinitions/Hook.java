9bee4ca17d712c501482a2bedabaef2e3ffbfe45
Alexandra Ostapenko <alexandraostapenko99@gmail.com>
2026-03-24 21:52:36 +0200

    Check number of posts on UI

--- ostapenkoProject/src/main/java/org/bdd/stepDefinitions/Hook.java
+++ ostapenkoProject/src/main/java/org/bdd/stepDefinitions/Hook.java
@@ -2,20 +2,39 @@ package org.bdd.stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.restassured.RestAssured;
+import org.api.dto.helpers.ApiHelper;
 import org.bdd.helpers.WebDriverHelper;
+import org.data.TestData;
+import org.pages.ParentPage;
 
 public class Hook {
     WebDriverHelper webDriverHelper;
+    ApiHelper apiHelper;
 
     public Hook(WebDriverHelper webDriverHelper) {
         this.webDriverHelper = webDriverHelper;
     }
 
-    @Before
-    public void setup() {}
+    @Before(order = 10)
+    public void setup() {
+        RestAssured.baseURI = ParentPage.baseUrl + "/api/";
+        apiHelper = new ApiHelper();
+    }
 
-    @After
+    @After(order = 15)
     public void tearDown() {
         webDriverHelper.quitDriver();
     }
+
+    @Before(value = "@deletePostsTillPresentForDefaultUser", order = 50)
+
+    @After(value = "@deletePostsTillPresentForDefaultUser", order = 50)
+    public void deletePostsTillPresent() {
+//        String username = TestData.currentUser;
+        String username = TestData.VALID_LOGIN_API;
+        apiHelper.deleteAllPostsTillPresent(username, apiHelper.getToken(username, TestData.VALID_PASSWORD_API));
+
+    }
+
 }
