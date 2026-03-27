128ac8a932345156d5a62c6a3571a2ccc846cd78
Moon4779 <dzhobolda23@gmail.com>
2026-03-27 13:20:00 +0100

    There is last HW-27-03

--- dzhoboldaTProject/src/main/java/org/bdd/stepDefinitions/LoginPageSteosDefenitions.java
+++ dzhoboldaTProject/src/main/java/org/bdd/stepDefinitions/LoginPageSteosDefenitions.java
@@ -9,6 +9,7 @@ import org.data.TestData;
 
 public class LoginPageSteosDefenitions extends MainSteps {
     public LoginPageSteosDefenitions(WebDriverHelper webDriverHelper) {
+
         super(webDriverHelper);
     }
 
@@ -45,5 +46,40 @@ public class LoginPageSteosDefenitions extends MainSteps {
     @Then("I see alert message with text {string}")
     public void iSeeAlertMessageWithText(String errorMessage) {
         pageProvider.getLoginPage().checkTextInAlertinCenter(errorMessage);
+    }
+    @When("I enter {string} into input UserName in Registration form on Login page")
+    public void iEnterUsernameIntoRegistrationForm(String userName) {
+        pageProvider.getLoginPage().enterTextIntoRegistrationUserNameField(userName);
+    }
+//
+//    @And("I enter {string} into input Email in Registration form on Login page")
+//    public void iEnterEmailIntoRegistrationForm(String email) {
+//        pageProvider.getLoginPage().enterTextIntoRegistrationEmailField(email);
+//    }
+
+//    @And("I enter {string} into input Password in Registration form on Login page")
+//    public void iEnterPasswordIntoRegistrationForm(String password) {
+//        pageProvider.getLoginPage().enterTextIntoRegistrationPasswordField(password);
+//    }
+
+//    @Then("I see error messages {string} in Registration form")
+//    public void iSeeErrorMessagesInRegistrationForm(String expectedMessages) {
+//        //  метод с SoftAssert
+//        pageProvider.getLoginPage().checkErrorsMessages(expectedMessages);
+//    }
+    @When("I enter {string} into {string} registration field")
+    public void i_enter_into_registration_field(String value, String fieldName) {
+        if (fieldName.equalsIgnoreCase("Username")) {
+            pageProvider.getLoginPage().enterTextIntoRegistrationUserNameField(value);
+        } else if (fieldName.equalsIgnoreCase("Email")) {
+            pageProvider.getLoginPage().enterTextIntoRegistrationEmailField(value);
+        } else if (fieldName.equalsIgnoreCase("Password")) {
+            pageProvider.getLoginPage().enterTextIntoRegistrationPasswordField(value);
+        }
+    }
+
+    @Then("I see error messages {string}")
+    public void i_see_error_messages(String messages) {
+        pageProvider.getLoginPage().checkErrorsMessages(messages);
     }
 }
