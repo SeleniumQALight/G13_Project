4c94f154f0513d7932399de4f944639aaf858dfc
Olena Boichuk <alyona.boychuk@gmail.com>
2026-04-07 21:08:31 +0300

    Exam project Api+UI

---
+++ boichukTAF/src/test/java/utils/TestData.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class TestData {
+    public static double apiBuy;
+    public static double apiSale;
+
+    public static double uiBuy;
+    public static double uiSale;
+}
